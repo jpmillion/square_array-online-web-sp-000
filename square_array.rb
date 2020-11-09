c6d19ee76e0a1e492f319d0e0829da5fdb9940ba
John Million <jpmillion@comcast.net>
2020-11-09 02:00:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
-  squared_array = []
+  #squared_array = []
   array.each do |element|
     squared_array.push(element**2)
   end
-  return squared_array
+  #return squared_array
 end
